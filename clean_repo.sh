6ba2c4f96be071a57fb0959806a42775811d7f84
Camille <camille.garosail@gmail.com>
2024-08-01 12:16:29 +0200

    Second commit, added gitignore

---
+++ clean_repo.sh
@@ -0,0 +1,16 @@
+rm cves/CVE-2024*
+rm cves/CVE-2023*
+rm cves/CVE-2022*
+rm cves/CVE-2021*
+rm cves/CVE-2020*
+rm cves/CVE-2019*
+rm cves/CVE-2018*
+rm cves/CVE-2017*
+rm cves/CVE-2016*
+rm cves/CVE-2015*
+rm cves/CVE-2014*
+rm cves/CVE-2013*
+rm cves/CVE-2012*
+rm cves/CVE-201*
+rm cves/*
+
