534c01121b8f66c028d8b8db049f7913063e2d04
Camille <camille.garosail@gmail.com>
2024-08-05 12:22:12 +0200

    automatic run

--- pg2json.py
+++ pg2json.py
@@ -19,15 +19,15 @@ with open("./conf.yaml") as stream:
             print("--- Fetching CVEs from database ---")
             start = time.time()
             # On va récupérer la table de CVEs
-            cveCursor.execute('''select 
-                                id, 
-                                name, 
-                                cwe, 
-                                modified, 
-                                published, 
-                                status, 
+            cveCursor.execute('''select
+                                id,
+                                name,
+                                cwe,
+                                modified,
+                                published,
+                                status,
                                 summary
-                            from cve 
+                            from cve
                             ORDER BY id DESC
                             ;''')
             # On itère donc sur chaque entrée de la table CVE cve
@@ -37,26 +37,26 @@ with open("./conf.yaml") as stream:
                     "id":        cve["id"],
                     "name":      cve["name"],
                     "cwe":       cve["cwe"],
-                    "modified":  cve["modified"].isoformat(), 
-                    "published": cve["published"].isoformat(), 
-                    "status":    cve["status"], 
+                    "modified":  cve["modified"].isoformat(),
+                    "published": cve["published"].isoformat(),
+                    "status":    cve["status"],
                     "summary":   cve["summary"]
                 }
-                
+
                 with conn.cursor(cursor_factory=RealDictCursor) as additionnalCursor:
-                    additionnalCursor.execute('''select 
+                    additionnalCursor.execute('''select
                                 cvss as cvss,
                                 exploitability_score as exploitability_score,
                                 impact_score as impact_score,
                                 cvss3_vector.value as vector
-                            from cvss3_mark 
+                            from cvss3_mark
                             join cvss3_vector on cvss3_mark.vector_id = cvss3_vector.id
                             WHERE cve_id = %s
                             ;''' % (cveTosave["id"]))
                     cvss3 =additionnalCursor.fetchone()
                     cveTosave["cvss3"] = cvss3
-                    
-                    additionnalCursor.execute('''select 
+
+                    additionnalCursor.execute('''select
                                 cvss2_mark.exploitability_score exploitability_score,
                                 cvss2_mark.impact_score as impact_score,
                                 cvss2_mark.cvss as cvss,
@@ -68,14 +68,14 @@ with open("./conf.yaml") as stream:
                     cvss2 = additionnalCursor.fetchone()
                     cveTosave["cvss2"] = cvss2
                     start = time.time_ns()
-                    additionnalCursor.execute('''select 
+                    additionnalCursor.execute('''select
                                     cots_id as id
                                 from cots_cves
-                                WHERE cves_id = %s 
+                                WHERE cves_id = %s
                                 ;''' % (cveTosave["id"]))
                     cpes = additionnalCursor.fetchall()
                     cveTosave["cpes"] = sorted(cpes, key=lambda d: d["id"])
-                try: 
+                try:
                     # On créé un fichier de nom cveName.json s'il n'existe pas déjà
                     with open(fileName, 'x') as fd:
                         json.dump(cveTosave, fd, indent=4)
@@ -86,7 +86,8 @@ with open("./conf.yaml") as stream:
                     if cveTosave != storedCVE:
                         with open(fileName, 'w') as fd:
                             json.dump(cveTosave, fd, indent=4)
-                subprocess.run(["git", "add", fileName])
+                
+        subprocess.run(["git", "add", "cves/*"])
 
         end = time.time()
         print("finished creating jsons for CVEs in %ds" % (end - start) )
@@ -94,20 +95,20 @@ with open("./conf.yaml") as stream:
             print("--- Fetching CPEs from database ---")
             start = time.time_ns()
             cotsCursor.execute('''select * from cots ORDER BY id;''')
-            
+
             for cots in cotsCursor:
                 fileName = "cots/" + str(cots["id"]) + ".json"
                 cotsToSave = {
                     "id":          cots["id"],
                     "name":        cots["name"],
                     "version":     cots["version"],
-                    "created_at":  cots["created_at"].isoformat(), 
-                    "updated_at":  cots["updated_at"].isoformat(), 
-                    "obsolete_at": cots["obsolete_at"], 
+                    "created_at":  cots["created_at"].isoformat(),
+                    "updated_at":  cots["updated_at"].isoformat(),
+                    "obsolete_at": cots["obsolete_at"],
                     "cpe":         cots["cpe"],
                     "to_analyse":  cots["to_analyse"],
                 }
-                try: 
+                try:
                     # On créé un fichier de nom cveName.json s'il n'existe pas déjà
                     with open(fileName, 'x') as fd:
                         json.dump(cotsToSave, fd, indent=4)
@@ -118,7 +119,8 @@ with open("./conf.yaml") as stream:
                     if cotsToSave != storedCots:
                         with open(fileName, 'w') as fd:
                             json.dump(cotsToSave, fd, indent=4)
-                subprocess.run(["git", "add", fileName])
+                
+        subprocess.run(["git", "add", "cpes/*"])
 
         end = time.time_ns()
         print("finished creating jsons for CPEs in %ds" % ((end - start)/1000000000) )
@@ -126,24 +128,24 @@ with open("./conf.yaml") as stream:
             print("--- Fetching CPEs from database ---")
             start = time.time_ns()
             analysisCursor.execute('''select * from applicability_analysis ORDER BY id;''')
-            
+
             for analysis in analysisCursor:
                 fileName = "analysis/" + str(analysis["id"]) + ".json"
                 analysisToSave = {
                     "id":              analysis["id"],
-                    "created_at":      analysis["created_at"].isoformat(), 
-                    "updated_at":      analysis["updated_at"].isoformat(), 
-                    "done":            analysis["done"], 
+                    "created_at":      analysis["created_at"].isoformat(),
+                    "updated_at":      analysis["updated_at"].isoformat(),
+                    "done":            analysis["done"],
                     "applicable":      analysis["applicable"],
                     "justification":   analysis["justification"],
                     "validated":       analysis["validated"],
                     "cots_id":         analysis["cots_id"],
-                    "cve_id":          analysis["cve_id"], 
-                    "first_for_id":    analysis["first_for_id"], 
+                    "cve_id":          analysis["cve_id"],
+                    "first_for_id":    analysis["first_for_id"],
                     "last_updater_id": analysis["last_updater_id"],
                     "hidden":          analysis["hidden"]
                 }
-                try: 
+                try:
                     # On créé un fichier de nom cveName.json s'il n'existe pas déjà
                     with open(fileName, 'x') as fd:
                         json.dump(analysisToSave, fd, indent=4)
@@ -153,13 +155,15 @@ with open("./conf.yaml") as stream:
                         storedAnalysis = json.load(fd)
                     if analysisToSave != storedAnalysis:
                         with open(fileName, 'w') as fd:
-                            json.dump(analysisToSave, fd, indent=4)  
-                subprocess.run(["git", "add", fileName])
+                            json.dump(analysisToSave, fd, indent=4)
+            subprocess.run(["git", "add", "analysis/*"])
             end = time.time_ns()
             print("finished creating jsons for Analyses in %ds" % ((end - start)/1000000000) )
+        print("Conversion done, now commiting and pushing evolution")
+        subprocess.run(["git", "commit", "-m", "automatic run"])
+        subprocess.run(["git", "push"])
 
     except():
         print("Could not find configuration file, make sure you have a conf.yaml file with the right configuration")
-        
-        
-        
+
+
